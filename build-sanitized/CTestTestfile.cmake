e0de4d2d1efba9cbdd184f5a0c2b1f0ff9895e70
Michał <michalplanetabiznes@gmail.com>
2026-07-02 14:46:43 +0200

    ci: removed tracked build cache and update checkout runtime (#11)

--- build-sanitized/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/skillu/dev/tickline
-# Build directory: /home/skillu/dev/tickline/build-sanitized
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[tickline_version_tests]=] "/home/skillu/dev/tickline/build-sanitized/tickline_version_tests")
-set_tests_properties([=[tickline_version_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/skillu/dev/tickline/CMakeLists.txt;82;add_test;/home/skillu/dev/tickline/CMakeLists.txt;0;")
