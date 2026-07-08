8986723cb44e052d094aa7e77100b9134980b56c
MichalPlanetaDev <michalplanetabiznes@gmail.com>
2026-07-08 03:27:16 +0200

    test: align C++ version assertions with v1.0.0

--- cpp/tests/version_tests.cpp
+++ cpp/tests/version_tests.cpp
@@ -7,8 +7,8 @@ int main()
 {
     constexpr auto version = tickline::project_version();
 
-    static_assert(version.major == 0);
-    static_assert(version.minor == 8);
+    static_assert(version.major == 1);
+    static_assert(version.minor == 0);
     static_assert(version.patch == 0);
     static_assert(version.channel.empty());
 
